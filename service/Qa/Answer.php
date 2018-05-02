edb1727c3b23f421f094a717fe04aefe60f25138
tian.zheng <tian.zheng@zol.com.cn>
2018-05-02 18:04:41 +0800

    设计接口

---
+++ service/Qa/Answer.php
@@ -0,0 +1,27 @@
+<?php
+namespace Service\Qa;
+
+class Answer
+{
+
+    public function add($userId, $qaId, $answer)
+    {
+
+    }
+
+    public function setStatus($userId, $answerId, $status)
+    {
+
+    }
+
+    public function getList($qaId, $count, $offset)
+    {
+
+    }
+
+    public function getBestList($qaId, $count, $offset)
+    {
+
+    }
+
+}
---
+++ service/Qa/Question.php
@@ -0,0 +1,31 @@
+<?php
+namespace Service\Qa;
+
+class Question
+{
+    public function searchByTags($tags, $count = 12, $offset = 0)
+    {
+
+    }
+
+    public function infoById($qaId, $withTags = true)
+    {
+
+    }
+
+    public function add($userId, $title, $summary, $tags = array())
+    {
+
+    }
+
+    public function update($qaId, $update)
+    {
+
+    }
+
+    public function setStatus($userId, $qaId, $status)
+    {
+
+    }
+}
+
